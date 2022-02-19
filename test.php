8cc35ec02a4116feea8e8d70b54e2ec027ecadc3
Manish Chauhan <61532686+manish532@users.noreply.github.com>
2022-02-19 12:33:53 +0530

    Update test.php

--- test.php
+++ test.php
@@ -3,6 +3,6 @@
 // ************How to print 1 to 100 without Loop************** 
     array_map(function ($value) {
           echo $value."</br>";
-          }, range(1, 11));
+          }, range(1, 100));
 
  ?>
